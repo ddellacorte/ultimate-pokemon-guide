d72363ed347f71c9c68dc472767dd336a56942fe
Domenico della Corte <ddellacorte2022@gmail.com>
2025-12-12 13:39:03 +0000

    feat(pokemon): enhance evolution chain data retrieval in Pokemon service

--- src/app/pages/pokemon/pokemon.service.ts
+++ src/app/pages/pokemon/pokemon.service.ts
@@ -97,8 +97,74 @@ export class PokemonService {
                 pokemons(where: {is_default:{_eq: false}}) {
                   is_default
                   name
-                  pokemonsprites {
-                    sprites 
+                  pokemontypes {
+                    type {
+                      typenames(where: {language_id: { _eq: ${languageId} }}) {
+                        name
+                      }
+                    }
+                  }
+                }   
+                capture_rate
+                pokemonspeciesflavortexts(where: {language_id: { _eq: 9 }}) {
+                  flavor_text
+                }
+                evolutionchain {
+                  pokemonspecies {
+                    evolution_chain_id
+                    id
+                    name
+                    evolves_from_species_id
+                    pokemonevolutions {
+                      pokemonspecy {
+                        id 
+                        name
+                        pokemons {
+                          pokemonforms {
+                            form_name
+                            name
+                          }
+                        }
+                      }
+                      PokemonspecyByPartySpeciesId {id name}
+                      region_id
+                      region {
+                        name
+                      }
+                      location {
+                        name
+                      }
+                      evolved_species_id
+                      min_level
+                      party_species_id
+                      base_form_id
+                      baseformid {
+                        id
+                        name
+                      }
+                      evolution_item_id
+                      item {
+                        id
+                        name
+                      }
+                      ItemByHeldItemId {
+                        name
+                      }
+                      type {
+                        name
+                      }
+                      region {
+                        name
+                      }
+                      gender {
+                        name
+                      }
+                      evolutiontrigger {
+                        evolutiontriggernames(where: {language_id: { _eq: 9 }}) {
+                          name
+                        }
+                      }
+                    }
                   }
                 }
                 capture_rate
